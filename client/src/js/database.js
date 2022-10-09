4883b2cdf8b0f0cbb68e0cea32625d9887714464
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-08 23:03:43 -0400

    getting all txt content from the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// Creating database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -7,7 +8,9 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { 
+        keyPath: 'id', autoIncrement: true 
+      });
       console.log('jate database created');
     },
   });
@@ -22,7 +25,14 @@ export const putDb = async (content) => {
   console.log(result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Getting all the text content from the database
+export const getDb = async (e) => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  return result?.value;
+};
 
 initdb();
