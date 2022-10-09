7a72f01add2f30da54d3d9534ccacea0614bc9c0
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-09 00:08:07 -0400

    click event handler for butInstall, trigger event 1x then disable and hide button.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,26 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+const triggerEvent = window.deferredPrompt;
+
+if(!triggerEvent) {
+    return;
+}
+
+triggerEvent.prompt();
+
+// to make the button a one time use function, disable and hide.
+window.deferredPrompt = null; 
+
+butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
